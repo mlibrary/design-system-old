190487ac0e441e76a28dfccbe85b0be15e61fc3b
Bridget <burkebri@umich.edu>
2019-12-04 15:16:09 -0500

    Add design tokens (#3)
    * Add scss partials and build.scss
    
    * Use @umich-lib/css with @umich-lib/components.
    
    * Add CSS gitignore.
    
    * Watch scss files.
    
    * Add tokens and build
    
    * update readme
    
    * Fix lerna run cmd hanging. Use parallel option.
    
    * Automate Theo with Gulp.
    
    * Include custom properties in the css dist as a single file.

--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -1,17 +1,18 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
 
 export const config: Config = {
-  namespace: 'components',
+  namespace: "components",
+  globalStyle: "./node_modules/@umich-lib/css/dist/umich-lib.css",
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader'
+      type: "dist",
+      esmLoaderPath: "../loader"
     },
     {
-      type: 'docs-readme'
+      type: "docs-readme"
     },
     {
-      type: 'www',
+      type: "www",
       serviceWorker: null // disable service workers
     }
   ]
--- packages/css/gulpfile.js
+++ packages/css/gulpfile.js
@@ -1,12 +1,51 @@
-const { src, dest, parallel } = require("gulp");
+const { src, dest, series, watch, task } = require("gulp");
 const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const theo = require("gulp-theo");
 
 sass.compiler = require("node-sass");
 
-function css() {
-  return src("src/*.scss")
+task("css:build-dist", function() {
+  return src("src/scss/main.scss")
     .pipe(sass())
-    .pipe(dest("dist"));
-}
+    .pipe(rename("umich-lib.css"))
+    .pipe(dest("dist/"));
+});
 
-exports.default = parallel(css);
+const scssWatcher = watch(["src/scss/*.scss"]);
+
+scssWatcher.on("change", function() {
+  task("css:build-dist");
+});
+
+task("tokens:scss", function() {
+  return src("src/tokens.json")
+    .pipe(
+      theo({
+        transform: { type: "web" },
+        format: { type: "scss" }
+      })
+    )
+    .pipe(rename("_tokens.scss"))
+    .pipe(dest("src/scss/"));
+});
+
+task("tokens:custom-properties", function() {
+  return src("src/tokens.json")
+    .pipe(
+      theo({
+        transform: { type: "web" },
+        format: { type: "custom-properties.css" }
+      })
+    )
+    .pipe(rename("_custom-properties.scss"))
+    .pipe(dest("src/scss"));
+});
+
+const build = series(
+  task("tokens:scss"),
+  task("tokens:custom-properties"),
+  task("css:build-dist")
+);
+
+exports.default = build;
