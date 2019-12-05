fe1c82feaa68d340bbe45eb01b080999f6028bf2
Jon Earley <jonathan.a.earley@gmail.com>
2019-12-05 13:00:32 -0500

    Update chat to use custom vars.

--- packages/components/src/components/chat/m-chat.tsx
+++ packages/components/src/components/chat/m-chat.tsx
@@ -1,58 +1,72 @@
-import { Component, h } from "@stencil/core";
+import { Component, State, Listen, h } from "@stencil/core";
+import cn from "classnames";
+
+function ChatIcon() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" class="m-chat__svg">
+      <path
+        d="M14.2,6.35A1.47,1.47,0,0,1,15.65,7.8v8.8a1.44,1.44,0,0,1-.43,1,1.37,1.37,0,0,1-1,.44H3.92L1,21V7.8a1.39,1.39,0,0,1,.42-1,1.36,1.36,0,0,1,1-.43Z"
+        fill="#ffcc06"
+      />
+      <path
+        d="M21.56,3A1.43,1.43,0,0,1,23,4.44V17.65l-2.92-2.93H9.8a1.33,1.33,0,0,1-1-.44,1.4,1.4,0,0,1-.43-1V4.44a1.36,1.36,0,0,1,.43-1A1.39,1.39,0,0,1,9.8,3Z"
+        fill="#10284b"
+      />
+    </svg>
+  );
+}
 
 @Component({
   tag: "m-chat",
   styleUrl: "m-chat.css"
 })
 export class Chat {
-  getSVG() {
-    return (
-      <svg viewBox="0 0 282.682 130.146" class="m-chat__svg">
-        <path
-          fill="#FFCF01"
-          d="M179.486,29.948c-41.058-21.774-107.634-21.774-148.689,0 c-41.062,21.773-41.062,57.076,0,78.851c3.595,1.905,7.393,3.639,11.354,5.209c-5.937,9.262-14.562,16.14-14.562,16.14 s18.048-0.609,32.208-10.465c39.087,9.902,87.309,6.283,119.689-10.884C220.548,87.023,220.548,51.721,179.486,29.948z"
-        />
-        <path
-          fill="#064473"
-          d="M268.192,0H110.389c-8.005,0-14.491,6.488-14.491,14.487v65.787 c0,8.005,6.486,14.489,14.491,14.489h60.09c13.492,16.812,41.679,20.023,41.679,20.023s-9.593-9.021-14.533-20.023h70.567 c8.009,0,14.489-6.484,14.489-14.489V14.487C282.682,6.488,276.2,0,268.192,0z"
-        />
-        <g fill="#FFF">
-          <path d="M140.418,59.408l-3.94,14.202h-12.971l16.911-55.33h16.418l17.157,55.33h-13.463l-4.27-14.202H140.418z M154.455,50.05l-3.448-11.739c-0.984-3.283-1.97-7.388-2.791-10.672h-0.164c-0.82,3.284-1.642,7.471-2.545,10.672l-3.283,11.739 H154.455z" />
-          <path d="M181.628,62.692c2.299,1.396,7.061,3.037,10.754,3.037c3.777,0,5.336-1.313,5.336-3.366 c0-2.052-1.23-3.037-5.91-4.597c-8.291-2.791-11.492-7.307-11.41-12.067c0-7.471,6.402-13.135,16.336-13.135 c4.68,0,8.865,1.067,11.328,2.298l-2.217,8.62c-1.805-0.985-5.254-2.299-8.701-2.299c-3.037,0-4.762,1.231-4.762,3.284 c0,1.888,1.561,2.873,6.486,4.597c7.634,2.627,10.836,6.485,10.918,12.396c0,7.47-5.911,12.971-17.404,12.971 c-5.254,0-9.933-1.149-12.971-2.792L181.628,62.692z" />
-        </g>
-        <path
-          fill="#FFF"
-          d="M232.064,50.378h0.164c0.902-1.642,1.888-3.283,2.873-4.843l8.127-12.068h15.022l-14.366,16.255 l16.419,23.889h-15.352l-9.687-16.501l-3.201,3.94V73.61h-12.479V15.325h12.479V50.378z"
-        />
-      </svg>
-    );
+  constructor() {
+    this.open = false;
+  }
+
+  @State() open: boolean;
+
+  toggle() {
+    this.open = !this.open;
   }
 
   render() {
     return (
-      <details class="m-chat__details">
-        <summary class="m-chat__summary">
-          {this.getSVG()}
+      <div
+        class={cn("m-chat", {
+          "m-chat--open": this.open
+        })}
+      >
+        <button
+          class="m-chat__button"
+          aria-expanded={String(this.open)}
+          onClick={() => this.toggle()}
+        >
+          <ChatIcon />
           <span class="m-chat__label">Ask a Librarian</span>
-          <div aria-hidden="true" class="m-chat__icon-container">
-            <svg
-              width="24px"
-              height="24px"
-              viewBox="0 0 24 24"
-              class="m-chat__open-icon"
-            >
-              <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"></path>
-            </svg>
-            <svg
-              width="24px"
-              height="24px"
-              viewBox="0 0 24 24"
-              class="m-chat__close-icon"
-            >
-              <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
-            </svg>
+          <div aria-hidden="true" class="m-chat__icons">
+            {this.open ? (
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                class="m-chat__open-icon"
+              >
+                <path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"></path>
+              </svg>
+            ) : (
+              <svg
+                width="24px"
+                height="24px"
+                viewBox="0 0 24 24"
+                class="m-chat__close-icon"
+              >
+                <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+              </svg>
+            )}
           </div>
-        </summary>
+        </button>
         <div class="m-chat__window">
           <iframe
             class="m-chat__iframe"
@@ -60,7 +74,7 @@ export class Chat {
             src="https://libraryh3lp.com/chat/umlibraryaskalibrarian@chat.libraryh3lp.com?skin=27279"
           />
         </div>
-      </details>
+      </div>
     );
   }
 }
