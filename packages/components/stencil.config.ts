e1d2f9ef0151971d0a581f0cc64cadbaebdc49c8
Jon Earley <jonathan.a.earley@gmail.com>
2020-01-29 15:46:21 -0500

    Setup for React distribution.

--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -1,10 +1,15 @@
 import { Config } from "@stencil/core";
+import { reactOutputTarget } from "@stencil/react-output-target";
 import { sass } from "@stencil/sass";
 
 export const config: Config = {
   namespace: "umich-lib",
   globalStyle: "./node_modules/@umich-lib/css/dist/umich-lib.css",
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: "@umich-lib/react",
+      proxiesFile: "../react/src/components.ts"
+    }),
     {
       type: "dist",
       esmLoaderPath: "../loader"
