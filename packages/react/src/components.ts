cf36110da44b985aebf8542b9e0c67f74e0dc3bf
Jon Earley <jonathan.a.earley@gmail.com>
2020-02-11 08:21:01 -0500

    .

---
+++ packages/react/src/components.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from '@umich-lib/react';
+
+import { defineCustomElements, applyPolyfills } from '@umich-lib/react/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const MChat = /*@__PURE__*/createReactComponent<JSX.MChat, HTMLMChatElement>('m-chat');
+export const MIcon = /*@__PURE__*/createReactComponent<JSX.MIcon, HTMLMIconElement>('m-icon');
