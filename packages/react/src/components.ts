12aab66c1c393ef5ff5ae41c5a428d985685db74
Jon Earley <jonathan.a.earley@gmail.com>
2020-02-22 11:53:41 -0500

    Save generated typescript to src then export to dist.

---
+++ packages/react/src/components.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from '@umich-lib/components';
+
+import { defineCustomElements, applyPolyfills } from '@umich-lib/components/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const MChat = /*@__PURE__*/createReactComponent<JSX.MChat, HTMLMChatElement>('m-chat');
+export const MIcon = /*@__PURE__*/createReactComponent<JSX.MIcon, HTMLMIconElement>('m-icon');
+export const MUniversalHeader = /*@__PURE__*/createReactComponent<JSX.MUniversalHeader, HTMLMUniversalHeaderElement>('m-universal-header');
