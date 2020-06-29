24543bce4cac943f57350f974a0450cca61a04c7
Jon Earley <jonathan.a.earley@gmail.com>
2020-06-29 13:35:36 -0400

    Prevent caching of website alerts.

--- packages/components/src/components/website-alerts/website-alerts.tsx
+++ packages/components/src/components/website-alerts/website-alerts.tsx
@@ -15,7 +15,7 @@ export class WebsiteAlerts {
     if (domain.includes("umich.edu")) {
       this.status = "loading";
 
-      fetch("https://staff.lib.umich.edu/api/alerts")
+      fetch("https://staff.lib.umich.edu/api/alerts", { cache: "no-cache" })
         .then(response => response.json())
         .then(result => {
           const alertsThatTargetHost = result.reduce((memo, alert) => {
