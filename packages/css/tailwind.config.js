bb02a537c1d41df37bdb645262ee42b7fd4ce266
Jon Earley <Jonathan.a.earley@gmail.com>
2021-02-12 19:35:23 -0500

    Remove tailwind + some cleanup. (#42)

--- packages/css/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-// tailwind.config.js
-// Docs: https://tailwindcss.com/docs/configuration
-const tailwindThemeMaize = require("./tailwind-theme-maize.js");
-
-module.exports = {
-  theme: tailwindThemeMaize,
-};
