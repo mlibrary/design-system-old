56129dc1985d900572c44f702cca3b72c170bbeb
Jon Earley <Jonathan.a.earley@gmail.com>
2021-03-25 08:56:21 -0400

    progress

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,15 @@ const theo = require("gulp-theo");
 const browsersync = require("browser-sync").create();
 const del = require("del");
 
-function clean() {
-  return del(["dist/**", "!dist"], { force: true });
+function clean(cb) {
+  (async () => {
+    const deletedDirectoryPaths = await del(["temp", "public"]);
+
+    console.log("Deleted directories:\n", deletedDirectoryPaths.join("\n"));
+  })();
+
+  cb();
 }
-clean.description = "Delete distribution directory ('dist/**') and files.";
 
 function tokens() {
   return src("design-tokens.json")
@@ -17,17 +22,13 @@ function tokens() {
         format: { type: "custom-properties.css" },
       })
     )
-    .pipe(dest("css"));
+    .pipe(dest("build/tokens"));
 }
-tokens.description =
-  "Build CSS custom properties from design-tokens.json with Theo.";
-
-function css() {
+function styles() {
   // The PostCSS configuration is loaded
   // automatically from postcss.config.js
-  return src("css/umich-lib.css").pipe(postcss()).pipe(dest("dist"));
+  return src("styles/umich-lib.css").pipe(postcss()).pipe(dest("build"));
 }
-css.description = "Build umich-lib.css with PostCSS.";
 
 function browser(cb) {
   browsersync.init({
@@ -38,18 +39,16 @@ function browser(cb) {
   });
   cb();
 }
-browser.description = "Start the developer workshop webpage.";
 
 function reload(cb) {
   browsersync.reload();
   cb();
 }
-reload.description = "Reload the developer workshop webpage.";
 
 function watcher() {
-  watch("css/*.css", series(css, reload));
+  watch("styles/*.css", series(styles, reload));
   watch("developer-workshop.html", reload);
   watch("design-tokens.json", series(tokens, reload));
 }
 
-exports.default = series(clean, tokens, css, browser, watcher);
+exports.default = series(clean, tokens, styles, browser, watcher);
--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from "@stencil/core";
 
 export const config: Config = {
   namespace: "umich-lib",
-  srcDir: "src/components",
+  srcDir: "components",
   outputTargets: [
     {
       type: 'dist',
